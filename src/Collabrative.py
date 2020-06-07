dc56c84305fcf1066f6b7fed3bf815ab83feef8c
Janki Chauhan <janki.chauhan@gmail.com>
2020-06-07 16:04:44 -0700

    Minor changes

--- src/Collabrative.py
+++ src/Collabrative.py
@@ -5,10 +5,11 @@ import mysql.connector
 from fastai.collab import *
 import os.path
 from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
 
 config = {
     'user': '',
-    'password': '',
+    'password': '!',
     'host': '127.0.0.1',
     'database': 'recommender',
     'raise_on_warnings': True
@@ -27,18 +28,25 @@ class Collabrative:
     def transform(self):
         print(" in transform")
 
-        sql_query = "select id,user,rating,comment,ID,name from user_ratings"
+        sql_query = "select u.user_id as id,u.username as user,u.ratings as rating,u.review as comment,u.game_id as game_id,u.gamename as name from user_ratings"
         self.user_ratings = pd.read_sql_query(sql_query, cnx)
-
         games_by_users = self.user_ratings.groupby('name')['rating'].agg(['mean', 'count']).sort_values('mean',
-                                                                                                        ascending=False)
+                                                                                                       ascending=False)
         games_by_users['rank'] = games_by_users.reset_index().index + 1
 
         data_user_rating = CollabDataBunch.from_df(self.user_ratings, user_name='user', item_name='name',
                                                    rating_name='rating', bs=100000,
                                                    seed=42)
-        learner = collab_learner(data_user_rating, n_factors=50, y_range=(2., 10))
-        learner.fit_one_cycle(3, 1e-2, wd=0.15)
+        learner = collab_learner(data_user_rating, n_factors=50, y_range=(2., 10), metrics=[root_mean_squared_error])
+
+        lr_find(learner)
+        learner.recorder.plot()
+        plt.show()
+
+        learner.fit_one_cycle(5, 1e-2, wd=0.15)
+
+        learner.recorder.plot_losses()
+        plt.show()
 
         top_games = games_by_users[games_by_users['count'] > self.COUNT_LIMIT].sort_values('mean',
                                                                                            ascending=False).reset_index()
@@ -88,3 +96,5 @@ class Collabrative:
         recommendation_df = top_games_df.iloc[indices[0][:10]].sort_values('model_score', ascending=False)
 
         return recommendation_df[['name', 'mean', 'model_score']]
+
+# Collabrative().get_recommendations('Gloomhaven')
